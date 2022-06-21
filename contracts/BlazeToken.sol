10d9e1390989a544bd566c751085ef512a93e089
Ashish Khatri <khatri8.ashish@gmail.com>
2022-06-21 18:08:25 +0530

    Made BlazeToken contract upgradeable.

--- contracts/BlazeToken.sol
+++ contracts/BlazeToken.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
-contract BlazeToken is ERC20, Ownable {
+contract BlazeToken is ERC20Upgradeable, OwnableUpgradeable {
 
     event BlazeTokensMinted(address indexed to, uint256 indexed amount);
-    constructor() ERC20("BlazeToken", "BLZ") {}
+    function initialize() public initializer {
+        __ERC20_init("BlazeToken", "BLZ");
+    }
 
     function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
