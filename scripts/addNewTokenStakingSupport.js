45c01d667fea770a43f3a7675a94b874bef0f77c
Ashish Khatri <khatri8.ashish@gmail.com>
2022-06-23 18:26:35 +0530

    Updated script for new token staking support.

--- scripts/addNewTokenStakingSupport.js
+++ scripts/addNewTokenStakingSupport.js
@@ -3,21 +3,19 @@ const { ethers } = require("hardhat");
 async function main() {
     const [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-    const STAKING_MULTI_V1_PROXY_ADDRESS = "";
+    const STAKING_MULTI_V1_PROXY_ADDRESS = "0x35E901deCf363Fd7C5c14cFE5A2DB2EC2DdFb39c";
     const USDC_Token = "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b";
     const USDC_Price_Feed = "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB";
 
     // Get the upgradable Staking_Multi_V2 contract.
-    const Staking_Multi_V2 = await ethers.getContractFactory("Staking_Multi_V2");
-    const staking_Multi_V2 = await Staking_Multi_V2.attach(STAKING_MULTI_V1_PROXY_ADDRESS);
+    const Staking_Multi_V1 = await ethers.getContractFactory("Staking_Multi_V1");
+    const staking_Multi_V1 = await Staking_Multi_V1.attach(STAKING_MULTI_V1_PROXY_ADDRESS);
 
-    await staking_Multi_V2.addTokenSupport(
+    await staking_Multi_V1.addTokenSupport(
         USDC_Token, 
         USDC_Price_Feed
     );
     
-    // Check whether support for USDC tokens are supported for staking or not.
-    console.log(await staking_Multi_V2.supportedTokens(1));
 }
 
 main()
